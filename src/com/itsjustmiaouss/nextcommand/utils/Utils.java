a6b868f78353efce2d4f504ffdaa6717224545a3
ItsJustMiaouss <roman.doyelle@gmail.com>
2020-10-29 23:55:39 +0100

    Optimizations and addition of a clear command

--- src/com/itsjustmiaouss/nextcommand/utils/Utils.java
+++ src/com/itsjustmiaouss/nextcommand/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.itsjustmiaouss.nextcommand.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -11,6 +13,13 @@ public class Utils {
 
     public static boolean hasPermissionSender(CommandSender s, String permission){
         return s.hasPermission(permission) || s.hasPermission("nextcommand.*");
+    }
+
+    public static OfflinePlayer getOfflinePlayer(String name) {
+        for(OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+            if(player.getName().equals(name)) return player;
+        }
+        return null;
     }
 
 }
